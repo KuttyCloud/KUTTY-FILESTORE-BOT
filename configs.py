2727b875fb2aa763d9a917a72b03105433e6f246
KuttyCloud <kuttybotz54@gmail.com>
2024-11-16 08:40:28 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,19 +4,19 @@ import os
 
 
 class Config(object):
-	API_ID = 1923471
-	API_HASH = "fcdc178451cd234e63faefd38895c991"
-	BOT_TOKEN = "7031870365:AAGVTsJcD7JChbTUMQBepqRCS_gmGGpjwus"
-	BOT_USERNAME = "publicmoviesharebot"
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001814192750"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "880087645"))
+	API_ID = 17833358
+	API_HASH = "b52cd2b77ec1833ec5b96a9d70a363bb"
+	BOT_TOKEN = "7236258270:AAGs32-LFYKmFzea4HNtKzNy3eu5MBbHSlE"
+	BOT_USERNAME = "KuttyFileSharingBot"
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002105619071"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1572929036"))
 	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://leecher:leecher@cluster0.606mkpi.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "moviekoodu")
-	LOG_CHANNEL = -1001814192750
+	LOG_CHANNEL = -1002192881598
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is Permanent Files Store Bot!
